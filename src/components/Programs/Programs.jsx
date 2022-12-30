3eadaec685785543a5f669767d46ee3fe90b8276
sachin <007mishrasachinmishra@gmail.com>
2022-12-31 00:00:12 +0530

    added content in programs

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-import './Programs.css'
+import './Program.css'
+import {pprogramData} from '../../data/programsData.js'
 const Programs = () => {
   return (
-    <div>
+    <div className='Programs' id='programs'>
+        {/* header */}
+
+        <div className="programs-head">
+            <span className='stroke-text'>explore Our</span>
+            <span>Programs</span>
+            <span className='stroke-text'>to shape You</span>
+        </div>
       
     </div>
   )
