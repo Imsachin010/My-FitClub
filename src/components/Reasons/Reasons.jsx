0ea11ce03e560b0525e7d741dde58bd0b9104db3
sachin <007mishrasachinmishra@gmail.com>
2023-01-06 00:43:01 +0530

    cascading the reason section

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Reasons.css'
-import image1
+import image1 from "../../assets/image1.png"
+import image2 from "../../assets/image2.png"
+import image3 from "../../assets/image3.png"
+import image4 from "../../assets/image4.png"
 const Reasons = () => {
   return (
     <div>
