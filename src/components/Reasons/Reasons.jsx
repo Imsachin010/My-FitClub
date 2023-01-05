993bcc57bc48e231bcebd25ae989a501a39ce622
sachin <007mishrasachinmishra@gmail.com>
2023-01-06 00:52:21 +0530

    CSS for reasonsection

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -4,6 +4,9 @@ import image1 from "../../assets/image1.png"
 import image2 from "../../assets/image2.png"
 import image3 from "../../assets/image3.png"
 import image4 from "../../assets/image4.png"
+import nb from "../../assets/nb.png"
+import adidas from "../../assets/adidas.png"
+import tick from "../../assets/tick.png"
 const Reasons = () => {
   return (
     <div>
@@ -15,7 +18,10 @@ const Reasons = () => {
             <img src={image4} alt="" />
         </div>
         <div className="right-r">
-
+            <span>Some reasons</span>
+            <div>
+                <span>Why </span><span>Choose Us?</span>
+            </div>
         </div>
       </div>
     </div>
