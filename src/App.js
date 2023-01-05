8c65ebe12473bb50c56b1b1218827926eeec55a2
sachin <007mishrasachinmishra@gmail.com>
2023-01-06 00:17:31 +0530

    Added reason files and linked into app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
           <Hero/>
           <Programs/>
+          <Reasons/>
     </div>
   );
 
---
+++ src/components/Reasons/Reasons.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Reasons.css'
+const Reasons = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Reasons
