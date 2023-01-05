a5598aff38bdc21dfd6be6f6fc79a2f529e80873
sachin <007mishrasachinmishra@gmail.com>
2023-01-06 00:39:54 +0530

    added images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
+import Reasons from './components/Reasons/Reasons';
 function App() {
   return (
     <div className="App">
--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Reasons.css'
+import image1
 const Reasons = () => {
   return (
     <div>
-      
+      <div className="Reasons" id="reasons">
+        <div className="left-r">
+            <img src={image1} alt="" />
+            <img src={image2} alt="" />
+            <img src={image3} alt="" />
+            <img src={image4} alt="" />
+        </div>
+        <div className="right-r">
+
+        </div>
+      </div>
     </div>
   )
 }
