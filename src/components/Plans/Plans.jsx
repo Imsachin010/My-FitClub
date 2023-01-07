529d0219839c3cf88466f45d1750326c7d3aa0b0
sachin <007mishrasachinmishra@gmail.com>
2023-01-08 00:28:39 +0530

    Created plans pages

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Plans.css';
+const Plans = () => {
+  return (
+    <div className='plans-container'>
+      <div className="programs-header">
+        <span>READY TO START</span>
+        <span>YOUR JOURNEY</span>
+        <span>NEW WITH US</span>
+      </div>
+    </div>
+  )
+}
+
+export default Plans
