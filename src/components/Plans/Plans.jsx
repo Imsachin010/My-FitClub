63519ec403ee4a3fecf2464f09c53f24d595b30b
sachin <007mishrasachinmishra@gmail.com>
2023-01-13 23:57:06 +0530

    plans

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Plans.css';
+import {plansData} from '../../data/plansData'
 const Plans = () => {
   return (
     <div className='plans-container'>
       <div className="programs-header">
         <span>READY TO START</span>
         <span>YOUR JOURNEY</span>
-        <span>NEW WITH US</span>
+        <span className="stroke-text">NEW WITH US</span>
+      </div>
+      {/* plans card */}
+      <div className="plans">
+        {plansData.map((plan, 1) => (
+            <div className="plan">
+              {plan.icon}
+              <span>[plan.name]</span>
+              <span>[plan.name]</span>
+            </div>        
+        ))}
       </div>
     </div>
   )
