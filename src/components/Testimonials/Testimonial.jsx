0caa17e83c6aa60b07c46fcc775a8a43d106e5ab
sachin <007mishrasachinmishra@gmail.com>
2023-01-29 00:17:49 +0530

    hooked the testrimonial logic

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Testimonial.css'
+import {testimonialsData} from '../../data/testimonialsData';
+import { useState } from 'react';
 export const Testimonial = () => {
+    const [selected, setSelected] = useState(0)
+    const tlength = testimonialsData.length
   return (
     <div className='Testimonials'>
         <div className="left-t">
             <span>Testimonials</span>
             <span className='stroke-text'>What they</span>
             <span>Say About Us</span>
+            <span>
+                {testimonialsData[selected].review}
+            </span>
         </div>
         <div className="left-r"></div>
     </div>
